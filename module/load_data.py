15eb207e93774ee768f8b77d48813ae5c1b5c7c6
Katlyn <katlyn880815@gmail.com>
2023-10-10 23:27:28 +0800

    using mysql.connector.Error to checkout the error on EC2

--- module/load_data.py
+++ module/load_data.py
@@ -2,21 +2,29 @@ from module import db_connection as db
 import mysql.connector
 
 def load_data(sql, params = '', data_count = 'various'):
-    con = db.connection_pool.get_connection()
-    # cursor = con.cursor(dictionary = True)
-    if(params == ''):
-        cursor = con.cursor(dictionary = True)
-        cursor.execute(sql)
-    else:
-        cursor = con.cursor(dictionary = True)
-        cursor.execute(sql, (params))
-    if(data_count == 'various'):
-        result = cursor.fetchall()
-    elif(data_count == 'one'):
-        result = cursor.fetchone()
-        print(result)
-    con.close()
-    return result
+    try:
+        con = db.connection_pool.get_connection()
+        # cursor = con.cursor(dictionary = True)
+        if(params == ''):
+            cursor = con.cursor(dictionary = True)
+            cursor.execute(sql)
+        else:
+            cursor = con.cursor(dictionary = True)
+            cursor.execute(sql, (params))
+        if(data_count == 'various'):
+            result = cursor.fetchall()
+        elif(data_count == 'one'):
+            result = cursor.fetchone()
+            print(result)
+        return result
+    except mysql.connector.Error as e:
+        print(f"資料操作錯誤原因：{e}")
+        con.rollback()
+    finally:
+        if(con.is_connected()):
+            cursor.close()
+            con.close()
+    
 
 def cud_data(sql, params):
     con = db.connection_pool.get_connection()
